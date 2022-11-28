14420d39d730a68bbbf1c39b63d937e92371e267
unknown <Abduallah.imu2@gmail.com>
2022-11-28 14:25:39 +0300

    create http server

--- backEnd/index.js
+++ backEnd/index.js
@@ -5,4 +5,6 @@ http.createServer((request, response) => {
     response.writeHead(200, {
         'Content-Type': 'text/plain'
     })
-})
+    response.write('Hello world ')
+    response.end()
+}).listen(1337)
